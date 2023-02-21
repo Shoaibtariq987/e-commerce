6961128227c6bbdf41a8cb756a18e7e262d907a6
Shoaibtariq987 <114915491+Shoaibtariq987@users.noreply.github.com>
2023-02-21 17:25:05 +0500

    add files

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import "./App.css";
+
+function App() {
+  return (
+    
+<div class="card">
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Product Image</th>
+      <th scope="col">Product Name</th>
+      <th scope="col">Product Price</th>
+      <th scope="col">Product Discount Price</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td><img src="pic.jpg"/></td>
+      <td>Otto</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td><img src="pic.jpg"/></td>
+      <td>Thornton</td>
+      <td>@fat</td>
+      <td>Otto</td>
+      
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td><img src="pic.jpg"/></td>
+      <td>@twitter</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td><img src="pic.jpg"/></td>
+      <td>@twitter</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+  )
+}
+
+export default App;
